116282db656883085ac7a6a0ac99f92bbd92e36c
stevebillings <sbillings@blackducksoftware.com>
2016-04-12 11:40:16 -0400

    6: Change DB columns type to varchar

--- src/main/resources/VulnNaiAudit_DB_Create.sql
+++ src/main/resources/VulnNaiAudit_DB_Create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE vuln_nai_audit (
-	application_id CHAR(32) NOT NULL,
-	request_id CHAR(32) NOT NULL,
-	component_id CHAR(32) NOT NULL,
-	vulnerability_id CHAR(32) NOT NULL,
-	nai_audit_status CHAR(32),
-	nai_audit_comment CHAR(512)
+	application_id VARCHAR(32) NOT NULL,
+	request_id VARCHAR(32) NOT NULL,
+	component_id VARCHAR(32) NOT NULL,
+	vulnerability_id VARCHAR(32) NOT NULL,
+	nai_audit_status VARCHAR(32),
+	nai_audit_comment VARCHAR(512)
 );
 commit;
