87f9b5c5760df94e28a7f347fce06e3ed549c2e3
stevebillings <sbillings@blackducksoftware.com>
2016-04-20 19:18:31 -0400

    6: Added unit test for edit nai audit details controller

---
+++ src/test/java/com/blackducksoftware/tools/appedit/mocks/MockVulnNaiAuditDetailsService.java
@@ -0,0 +1,70 @@
+package com.blackducksoftware.tools.appedit.mocks;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.blackducksoftware.tools.appedit.exception.AppEditException;
+import com.blackducksoftware.tools.appedit.naiaudit.model.AppCompVulnComposite;
+import com.blackducksoftware.tools.appedit.naiaudit.model.AppCompVulnDetails;
+import com.blackducksoftware.tools.appedit.naiaudit.model.AppCompVulnKey;
+import com.blackducksoftware.tools.appedit.naiaudit.model.VulnNaiAuditDetails;
+import com.blackducksoftware.tools.appedit.naiaudit.service.VulnNaiAuditDetailsService;
+import com.blackducksoftware.tools.connector.codecenter.application.ApplicationPojo;
+import com.blackducksoftware.tools.connector.codecenter.common.AttributeValuePojo;
+import com.blackducksoftware.tools.connector.codecenter.common.VulnerabilitySeverity;
+import com.blackducksoftware.tools.connector.common.ApprovalStatus;
+
+public class MockVulnNaiAuditDetailsService implements
+	VulnNaiAuditDetailsService {
+
+    @Override
+    public ApplicationPojo getApplicationByNameVersion(String appName,
+	    String appVersion) throws AppEditException {
+
+	return new ApplicationPojo(appName, appName, appVersion,
+		new ArrayList<AttributeValuePojo>(), ApprovalStatus.APPROVED,
+		false, "owner");
+    }
+
+    @Override
+    public ApplicationPojo getApplicationById(String appId)
+	    throws AppEditException {
+	return new ApplicationPojo(appId, appId, "Unspecified",
+		new ArrayList<AttributeValuePojo>(), ApprovalStatus.APPROVED,
+		false, "owner");
+    }
+
+    @Override
+    public List<AppCompVulnComposite> getAppCompVulnCompositeList(
+	    String applicationId) throws AppEditException {
+
+	Date now = new Date();
+
+	AppCompVulnKey key = new AppCompVulnKey("app1Id", "request1Id",
+		"component1Id", "vulnerability1Id");
+	AppCompVulnDetails ccPart = new AppCompVulnDetails(key,
+		"component1Name", "component1Version", "vulnerability1Name",
+		VulnerabilitySeverity.HIGH, "vulnerability1BaseScore",
+		"vulnerability1ExploitableScore", "vulnerability1ImpactScore",
+		now, now, now, "vulnerability1Description", now, now,
+		"vulnerability1RemediationStatus",
+		"vulnerability1RemediationComments");
+	VulnNaiAuditDetails auditPart = new VulnNaiAuditDetails(key,
+		"vulnerability1NaiAuditStatus", "vulnerability1NaiAuditComment");
+
+	AppCompVulnComposite appCompVulnComposite = new AppCompVulnComposite(
+		key, ccPart, auditPart);
+	List<AppCompVulnComposite> result = new ArrayList<AppCompVulnComposite>();
+	result.add(appCompVulnComposite);
+	return result;
+    }
+
+    @Override
+    public AppCompVulnComposite updateVulnNaiAuditDetails(
+	    AppCompVulnComposite appCompVulnComposite) throws AppEditException {
+
+	return appCompVulnComposite;
+    }
+
+}
---
+++ src/test/java/com/blackducksoftware/tools/appedit/web/controller/EditNaiAuditDetailsControllerTest.java
@@ -0,0 +1,103 @@
+package com.blackducksoftware.tools.appedit.web.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+import com.blackducksoftware.tools.appedit.core.AppEditConfigManager;
+import com.blackducksoftware.tools.appedit.mocks.MockVulnNaiAuditDetailsService;
+import com.blackducksoftware.tools.appedit.naiaudit.model.AppCompVulnComposite;
+import com.blackducksoftware.tools.appedit.naiaudit.model.NaiAuditViewData;
+import com.blackducksoftware.tools.appedit.naiaudit.service.VulnNaiAuditDetailsService;
+
+public class EditNaiAuditDetailsControllerTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Test
+    public void test() throws Exception {
+
+	// Setup
+
+	EditNaiAuditDetailsController controller = new EditNaiAuditDetailsController();
+	Properties props = new Properties();
+	props.setProperty("cc.server.name", "not used");
+	props.setProperty("cc.user.name", "not used");
+	props.setProperty("cc.user.name", "not used");
+	props.setProperty("cc.password", "not used");
+	props.setProperty("attr.0.label", "not used");
+	props.setProperty("attr.0.ccname", "not used");
+	props.setProperty("attr.0.regex", "not used");
+	props.setProperty("field.input.validation.regex.username",
+		"\\[A-Za-z0-9@_.-\\]+");
+	props.setProperty("field.input.validation.regex.psw", ".+");
+	props.setProperty("app.version", "Unspecified");
+
+	props.setProperty("db.server", "not used");
+	props.setProperty("auditor.role.name", "Auditor");
+	props.setProperty("nai.audit.status.0", "Unreviewed");
+	props.setProperty("nai.audit.status.1", "Approved");
+	props.setProperty("nai.audit.status.2", "Rejected");
+
+	props.setProperty("nai.audit.rejected.status.name", "Rejected");
+	props.setProperty("nai.audit.rejected.status.changes.rem.status.to",
+		"NAI Rejected By Auditor");
+	props.setProperty("nai.audit.date.format", "MMM-dd-yyyy hh:mm:ss z");
+
+	props.setProperty("auditor.role.name", "not used");
+
+	AppEditConfigManager config = new AppEditConfigManager(props);
+
+	controller.setConfig(config);
+
+	VulnNaiAuditDetailsService vulnNaiAuditDetailsService = new MockVulnNaiAuditDetailsService();
+	controller.setVulnNaiAuditDetailsService(vulnNaiAuditDetailsService);
+
+	// Test
+	MockHttpServletRequest httpServletRequest = new MockHttpServletRequest(
+		"GET", "/editnaiauditdetails");
+
+	httpServletRequest.setParameter("appId", "app1Id");
+	WebRequest webRequest = new ServletWebRequest(httpServletRequest);
+	webRequest.setAttribute("appId", "app1Id", WebRequest.SCOPE_REQUEST);
+	Model model = new ExtendedModelMap();
+	controller.showNaiAuditDetails(webRequest, model);
+
+	// Verify
+
+	Map<String, Object> modelMap = model.asMap();
+	NaiAuditViewData naiAuditViewData = (NaiAuditViewData) modelMap
+		.get("selectedVulnerabilities");
+	assertEquals("app1Id", naiAuditViewData.getApplicationId());
+	assertEquals("app1Id", naiAuditViewData.getApplicationName());
+	assertEquals(null, naiAuditViewData.getComment());
+	assertEquals(null, naiAuditViewData.getVulnerabilityNaiAuditStatus());
+	assertEquals(null, naiAuditViewData.getItemList());
+
+	List<AppCompVulnComposite> vulnNaiAuditDetailsList = (List<AppCompVulnComposite>) modelMap
+		.get("vulnNaiAuditDetailsList");
+
+	assertEquals("component1Name", vulnNaiAuditDetailsList.get(0)
+		.getCcPart().getComponentName());
+	assertEquals("vulnerability1Name", vulnNaiAuditDetailsList.get(0)
+		.getCcPart().getVulnerabilityName());
+    }
+
+}
