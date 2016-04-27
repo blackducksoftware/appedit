e7fbf2f67fde314d0759c286dd22dc7a54a227c0
stevebillings <sbillings@blackducksoftware.com>
2016-04-27 18:19:00 -0400

    6: Last commit was wrong copyright. This one fixes it.

--- src/main/java/com/blackducksoftware/tools/appedit/naiaudit/dao/jdbc/JdbcVulnNaiAuditDetailsDao.java
+++ src/main/java/com/blackducksoftware/tools/appedit/naiaudit/dao/jdbc/JdbcVulnNaiAuditDetailsDao.java
@@ -1,12 +1,19 @@
 /*******************************************************************************
- * Copyright (c) 2016 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2016 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
  *
- * Contributors:
- *     IBM Corporation - initial API and implementation
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2 only
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License version 2
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *******************************************************************************/
 package com.blackducksoftware.tools.appedit.naiaudit.dao.jdbc;
 
