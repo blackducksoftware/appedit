c6b6d0f3fbdb12d79c68c179a5acaf96100fc043
stevebillings <sbillings@blackducksoftware.com>
2016-04-28 11:43:58 -0400

    6: Added key constraint to nai audit table

--- src/main/resources/VulnNaiAudit_DB_Create.sql
+++ src/main/resources/VulnNaiAudit_DB_Create.sql
@@ -4,7 +4,8 @@ CREATE TABLE vuln_nai_audit (
 	component_id VARCHAR(32) NOT NULL,
 	vulnerability_id VARCHAR(32) NOT NULL,
 	nai_audit_status VARCHAR(32) NOT NULL,
-	nai_audit_comment VARCHAR(512) NOT NULL
+	nai_audit_comment VARCHAR(512) NOT NULL,
+	CONSTRAINT unique_key UNIQUE ( application_id, request_id, component_id, vulnerability_id )
 );
 
 CREATE TABLE vuln_nai_audit_change_history (
