8e38c6314a2f7bbbf2587c4a62ec0be1677ba499
stevebillings <sbillings@blackducksoftware.com>
2016-07-06 16:03:16 -0400

    INTGEN-431: Added logging to authorization code

--- src/main/java/com/blackducksoftware/tools/appedit/core/dao/cc/CcUserAuthenticationDao.java
+++ src/main/java/com/blackducksoftware/tools/appedit/core/dao/cc/CcUserAuthenticationDao.java
@@ -51,191 +51,203 @@ import com.blackducksoftware.tools.connector.codecenter.user.CodeCenterUserPojo;
  *
  */
 public class CcUserAuthenticationDao implements UserAuthenticationDao {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass()
-	    .getName());
+	private final Logger logger = LoggerFactory.getLogger(this.getClass()
+			.getName());
 
-    private AppEditConfigManager config;
+	private AppEditConfigManager config;
 
-    @Inject
-    public void setConfig(AppEditConfigManager config) {
-	this.config = config;
-    }
-
-    /**
-     * Default constructor
-     * 
-     * @throws Exception
-     */
-    public CcUserAuthenticationDao() throws Exception {
-	logger.debug("Default constructor called");
-    }
-
-    /**
-     * Construct with given config (used by integration test).
-     * 
-     * @param config
-     * @throws Exception
-     */
-    public CcUserAuthenticationDao(AppEditConfigManager config)
-	    throws Exception {
-	logger.debug("Config passed via constructor");
-	this.config = config;
-    }
-
-    /**
-     * Attempt to authenticate the given user.
-     */
-    @Override
-    public AuthenticationResult authenticate(String username, String password) {
-
-	AppEditConfigManager userSpecificConfig;
-	CodeCenterUserPojo user;
-	boolean isAuditor;
-	try {
-	    userSpecificConfig = createConfig(username, password);
-	    CodeCenterServerWrapper userSpecificCcsw = createCodeCenterServerWrapper(userSpecificConfig);
-	    user = getUser(userSpecificCcsw, username);
-	    isAuditor = isAuditor(userSpecificCcsw, username, user);
-	} catch (AuthenticationException e1) {
-	    logger.warn(e1.getMessage());
-	    return e1.getAuthResult();
+	@Inject
+	public void setConfig(final AppEditConfigManager config) {
+		this.config = config;
 	}
 
-	if (isAuditor) {
-	    logger.info("User " + username
-		    + " has been authorized as an Auditor");
-	    return new AuthenticationResult(user.getId(), username, true,
-		    "Login as Auditor was successful.", Role.ROLE_AUDITOR);
+	/**
+	 * Default constructor
+	 *
+	 * @throws Exception
+	 */
+	public CcUserAuthenticationDao() throws Exception {
+		logger.debug("Default constructor called");
 	}
 
-	logger.info("User " + username + " has been authorized as a User");
-	return new AuthenticationResult(user.getId(), username, true,
-		"Login as User was successful.", Role.ROLE_USER);
-    }
-
-    private boolean isAuditor(CodeCenterServerWrapper userSpecificCcsw,
-	    String username, CodeCenterUserPojo user)
-	    throws AuthenticationException {
-	// Now see if this user is an auditor
-	boolean isAuditor = false;
-	try {
-	    isAuditor = userIsAuditor(username, userSpecificCcsw);
-	} catch (CommonFrameworkException e) {
-	    String message = "Error attempting to authorize this user an an auditor: "
-		    + e.getMessage() + "; Authorizing this user as an end user";
-	    AuthenticationResult authResult = new AuthenticationResult(
-		    user.getId(), username, true,
-		    "Login as User was successful.", Role.ROLE_USER);
-	    throw new AuthenticationException(authResult, message);
-	}
-	return isAuditor;
-    }
-
-    private CodeCenterUserPojo getUser(
-	    CodeCenterServerWrapper userSpecificCcsw, String username)
-	    throws AuthenticationException {
-	// Authorize by performing an operation this user should be able to do
-	CodeCenterUserPojo user;
-	try {
-	    user = userSpecificCcsw.getUserManager().getUserByName(username);
-	} catch (CommonFrameworkException e) {
-	    String message = "Authorization failed: " + e.getMessage();
-	    AuthenticationResult authResult = new AuthenticationResult(null,
-		    null, false, message, Role.ROLE_NONE);
-	    throw new AuthenticationException(authResult, message);
-	} catch (SOAPFaultException e) {
-	    String message = "Authorization failed: " + e.getMessage();
-	    AuthenticationResult authResult = new AuthenticationResult(null,
-		    null, false, message, Role.ROLE_NONE);
-	    throw new AuthenticationException(authResult, message);
-	}
-	return user;
-    }
-
-    private CodeCenterServerWrapper createCodeCenterServerWrapper(
-	    ConfigurationManager userSpecificConfig)
-	    throws AuthenticationException {
-	CodeCenterServerWrapper userSpecificCcsw = null;
-	try {
-	    userSpecificCcsw = new CodeCenterServerWrapper(userSpecificConfig);
-	} catch (Exception e) {
-	    String message = "Authentication failed: " + e.getMessage();
-	    AuthenticationResult authResult = new AuthenticationResult(null,
-		    null, false, message, Role.ROLE_NONE);
-	    throw new AuthenticationException(authResult, message);
-	}
-	return userSpecificCcsw;
-    }
-
-    private AppEditConfigManager createConfig(String username, String password)
-	    throws AuthenticationException {
-	Properties userSpecificProps = (Properties) config.getProps().clone();
-	userSpecificProps.setProperty("cc.user.name", username); // change
-								 // username
-	userSpecificProps.setProperty("cc.password", password); // and password
-	userSpecificProps.setProperty("cc.password.isencrypted", "false");
-	AppEditConfigManager userSpecificConfig;
-	try {
-	    userSpecificConfig = new AppEditConfigManager(userSpecificProps);
-	} catch (Exception e1) {
-	    String message = "Authentication failed: " + e1.getMessage();
-	    AuthenticationResult authResult = new AuthenticationResult(null,
-		    null, false, message, Role.ROLE_NONE);
-
-	    throw new AuthenticationException(authResult, message);
-	}
-	return userSpecificConfig;
-    }
-
-    private boolean userIsAuditor(String username,
-	    CodeCenterServerWrapper userSpecificCcsw)
-	    throws CommonFrameworkException {
-	if (!config.isEditNaiAuditEnabled()) {
-	    logger.info("The Edit NAI Audit feature has not been enabled");
-	    return false;
+	/**
+	 * Construct with given config (used by integration test).
+	 *
+	 * @param config
+	 * @throws Exception
+	 */
+	public CcUserAuthenticationDao(final AppEditConfigManager config)
+			throws Exception {
+		logger.debug("Config passed via constructor");
+		this.config = config;
 	}
 
-	List<UserRoleAssignment> roleAssignments;
-	UserNameToken userNameToken = new UserNameToken();
-	userNameToken.setName(username);
-	try {
-	    roleAssignments = userSpecificCcsw.getInternalApiWrapper()
-		    .getProxy().getRoleApi().getUserRoles(userNameToken);
-	} catch (SdkFault e) {
-	    throw new CommonFrameworkException("Error getting user's roles: "
-		    + e.getMessage());
+	/**
+	 * Attempt to authenticate the given user.
+	 */
+	@Override
+	public AuthenticationResult authenticate(final String username, final String password) {
+		logger.debug("authenticate(): Username: " + username);
+		AppEditConfigManager userSpecificConfig;
+		CodeCenterUserPojo user;
+		boolean isAuditor;
+		try {
+			userSpecificConfig = createConfig(username, password);
+			final CodeCenterServerWrapper userSpecificCcsw = createCodeCenterServerWrapper(userSpecificConfig);
+			user = getUser(userSpecificCcsw, username);
+			isAuditor = isAuditor(userSpecificCcsw, username, user);
+		} catch (final AuthenticationException e1) {
+			logger.warn(e1.getMessage());
+			return e1.getAuthResult();
+		}
+
+		if (isAuditor) {
+			logger.info("User " + username
+					+ " has been authorized as an Auditor");
+			return new AuthenticationResult(user.getId(), username, true,
+					"Login as Auditor was successful.", Role.ROLE_AUDITOR);
+		}
+
+		logger.info("User " + username + " has been authorized as a User");
+		return new AuthenticationResult(user.getId(), username, true,
+				"Login as User was successful.", Role.ROLE_USER);
 	}
-	boolean isAuditor = hasAuditorRole(userSpecificCcsw, username,
-		roleAssignments);
-	return isAuditor;
-    }
 
-    private boolean hasAuditorRole(CodeCenterServerWrapper userSpecificCcsw,
-	    String username, List<UserRoleAssignment> roleAssignments)
-	    throws CommonFrameworkException {
-
-	for (UserRoleAssignment roleAssignment : roleAssignments) {
-
-	    com.blackducksoftware.sdk.codecenter.role.data.Role ccRole;
-	    try {
-		ccRole = userSpecificCcsw.getInternalApiWrapper().getProxy()
-			.getRoleApi().getRole(roleAssignment.getRoleIdToken());
-	    } catch (SdkFault e) {
-		throw new CommonFrameworkException(
-			"Error getting details for user role "
-				+ roleAssignment.getRoleNameToken().getName()
-				+ ": " + e.getMessage());
-	    }
-	    if (ccRole.getRoleType() != RoleTypeEnum.OVERALL) {
-		continue;
-	    }
-	    logger.debug("User: " + username + "; CC Role: "
-		    + roleAssignment.getRoleNameToken().getName());
-	    if (config.getAuditorRoleName().equals(
-		    roleAssignment.getRoleNameToken().getName())) {
-		return true;
-	    }
+	private boolean isAuditor(final CodeCenterServerWrapper userSpecificCcsw,
+			final String username, final CodeCenterUserPojo user)
+					throws AuthenticationException {
+		// Now see if this user is an auditor
+		boolean isAuditor = false;
+		try {
+			isAuditor = userIsAuditor(username, userSpecificCcsw);
+		} catch (final CommonFrameworkException e) {
+			final String message = "Error attempting to authorize this user an an auditor: "
+					+ e.getMessage() + "; Authorizing this user as an end user";
+			final AuthenticationResult authResult = new AuthenticationResult(
+					user.getId(), username, true,
+					"Login as User was successful.", Role.ROLE_USER);
+			throw new AuthenticationException(authResult, message);
+		}
+		return isAuditor;
 	}
-	return false;
-    }
+
+	private CodeCenterUserPojo getUser(
+			final CodeCenterServerWrapper userSpecificCcsw, final String username)
+					throws AuthenticationException {
+		// Authorize by performing an operation this user should be able to do
+		logger.debug("getUser()");
+		CodeCenterUserPojo user;
+		try {
+			user = userSpecificCcsw.getUserManager().getUserByName(username);
+		} catch (final CommonFrameworkException e) {
+			final String message = "Authorization failed: " + e.getMessage();
+			final AuthenticationResult authResult = new AuthenticationResult(null,
+					null, false, message, Role.ROLE_NONE);
+			throw new AuthenticationException(authResult, message);
+		} catch (final SOAPFaultException e) {
+			final String message = "Authorization failed: " + e.getMessage();
+			final AuthenticationResult authResult = new AuthenticationResult(null,
+					null, false, message, Role.ROLE_NONE);
+			throw new AuthenticationException(authResult, message);
+		}
+		return user;
+	}
+
+	private CodeCenterServerWrapper createCodeCenterServerWrapper(
+			final ConfigurationManager userSpecificConfig)
+					throws AuthenticationException {
+		CodeCenterServerWrapper userSpecificCcsw = null;
+		try {
+			userSpecificCcsw = new CodeCenterServerWrapper(userSpecificConfig);
+		} catch (final Exception e) {
+			final String message = "Authentication failed: " + e.getMessage();
+			final AuthenticationResult authResult = new AuthenticationResult(null,
+					null, false, message, Role.ROLE_NONE);
+			throw new AuthenticationException(authResult, message);
+		}
+		return userSpecificCcsw;
+	}
+
+	private AppEditConfigManager createConfig(final String username, final String password)
+			throws AuthenticationException {
+		final Properties userSpecificProps = (Properties) config.getProps().clone();
+		userSpecificProps.setProperty("cc.user.name", username); // change
+		// username
+		userSpecificProps.setProperty("cc.password", password); // and password
+		userSpecificProps.setProperty("cc.password.isencrypted", "false");
+		AppEditConfigManager userSpecificConfig;
+		try {
+			userSpecificConfig = new AppEditConfigManager(userSpecificProps);
+		} catch (final Exception e1) {
+			final String message = "Authentication failed: " + e1.getMessage();
+			final AuthenticationResult authResult = new AuthenticationResult(null,
+					null, false, message, Role.ROLE_NONE);
+
+			throw new AuthenticationException(authResult, message);
+		}
+		return userSpecificConfig;
+	}
+
+	private boolean userIsAuditor(final String username,
+			final CodeCenterServerWrapper userSpecificCcsw)
+					throws CommonFrameworkException {
+		logger.debug("userIsAuditor(): Username: " + username);
+		if (!config.isEditNaiAuditEnabled()) {
+			logger.info("The Edit NAI Audit feature has not been enabled");
+			return false;
+		}
+
+		List<UserRoleAssignment> roleAssignments;
+		final UserNameToken userNameToken = new UserNameToken();
+		userNameToken.setName(username);
+		try {
+			logger.debug("SDK: Getting user's roles for user: " + username);
+			roleAssignments = userSpecificCcsw.getInternalApiWrapper()
+					.getProxy().getRoleApi().getUserRoles(userNameToken);
+			logger.debug("SDK: Done getting user's roles; role count: " + roleAssignments.size());
+		} catch (final SdkFault e) {
+			logger.debug("SDK: Error getting user's roles");
+			throw new CommonFrameworkException("Error getting user's roles: "
+					+ e.getMessage());
+		}
+		final boolean isAuditor = hasAuditorRole(userSpecificCcsw, username,
+				roleAssignments);
+		return isAuditor;
+	}
+
+	private boolean hasAuditorRole(final CodeCenterServerWrapper userSpecificCcsw,
+			final String username, final List<UserRoleAssignment> roleAssignments)
+					throws CommonFrameworkException {
+		logger.info("Checking user " + username + " to see if it has been assigned the auditor role");
+		for (final UserRoleAssignment roleAssignment : roleAssignments) {
+
+			com.blackducksoftware.sdk.codecenter.role.data.Role ccRole;
+			try {
+				logger.debug("SDK: Getting role for role ID: " + roleAssignment.getRoleIdToken().getId());
+				ccRole = userSpecificCcsw.getInternalApiWrapper().getProxy()
+						.getRoleApi().getRole(roleAssignment.getRoleIdToken());
+				logger.debug("SDK: Done getting role");
+			} catch (final SdkFault e) {
+				logger.debug("SDK: Error getting role");
+				throw new CommonFrameworkException(
+						"Error getting details for user role "
+								+ roleAssignment.getRoleNameToken().getName()
+								+ ": " + e.getMessage());
+			}
+			if (ccRole.getRoleType() != RoleTypeEnum.OVERALL) {
+				logger.debug("This role is not an overall role so cannot be the auditor role: "
+						+ ccRole.getName().getName());
+				continue;
+			}
+			logger.debug("User: " + username + "; CC Role: "
+					+ roleAssignment.getRoleNameToken().getName());
+			if (config.getAuditorRoleName().equals(
+					roleAssignment.getRoleNameToken().getName())) {
+				logger.info("Found auditor role; this user is an auditor");
+				return true;
+			}
+		}
+		logger.info("Did not find auditor role; this user is not an auditor");
+		return false;
+	}
 }
